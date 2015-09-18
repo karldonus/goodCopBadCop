3801101a14b9c7f025fb62374e6d65a8cee6f332
Karl Sakas <karldonus@gmail.com>
2015-09-18 01:23:07 -0400

    removed redundant files. rewrote up and down votes to tabulate independently. moved router outside to hopefully eliminate deploy concern. server client if statements to address similiar issue.

--- client/post/post.js
+++ client/post/post.js
@@ -1,11 +1,11 @@
 
 if(Meteor.isClient){
   Template.post.helpers({
-    'upcount': function(){
-      return UpVotes.find(this._id).count();
+    'upCount': function(){
+      return UpVotes.find({parent:this._id}).count();
     },
-    'downcount': function(){
-      return DownVotes.find(this._id).count();
+    'downCount': function(){
+      return DownVotes.find({parent:this._id}).count();
     },
     'postComments': function(){
       return Posts.find({parent:this._id});
@@ -18,11 +18,18 @@ Template.post.events({
   'keyup .comment':function(evt,tmpl){
     if(evt.which === 13){
       var commenttext = tmpl.find('.comment').value;
-      alert(commenttext);
       var options = {text:commenttext, parent:this._id};
       Meteor.call('addPost', options);
       $('.comment').val("").select().focus();
     }
+  },
+  'click .upVote':function(){
+    var options = {parent:this._id};
+    Meteor.call('addUpVote', options);
+  },
+  'click .downVote':function(){
+    var options = {parent:this._id};
+    Meteor.call('addDownVote', options);
   }
 });
 }
--- server/server.js
+++ server/server.js
@@ -1,27 +1,50 @@
-Meteor.publish('posts', function(userid){
-  return Posts.find({});
-});
-Meteor.publish('downvotes', function(){
-  return UpVotes.find({post:postid});
-});
-Meteor.publish('upvotes', function(){
-  return DownVotes.find({post:postid});
-});
+if(Meteor.isServer){
+  Meteor.publish('posts', function(userid){
+    return Posts.find({});
+  });
+}
+if(Meteor.isServer){
+  Meteor.publish('upvotes', function(){
+    return UpVotes.find({});
+  });
+}
+if(Meteor.isServer){
+  Meteor.publish('downvotes', function(){
+    return DownVotes.find({});
+  });
+}
+if(Meteor.isServer){
+  Meteor.methods({
+    //{text:'',owner:'',date:'',parent:''};
+    'addPost': function(options){
+      var post =     {
+          text: options.text,
+          owner: Meteor.userId(),
+          date: new Date(),
+          parent: options.parent
+        };
 
-Meteor.methods({
-  //{text:'',owner:'',date:'',parent:''};
-  'addPost': function(options){
-    var post =     {
-        text: options.text,
-        owner: Meteor.userId(),
-        date: new Date(),
-        parent: options.parent
+      Posts.insert(post);
+      console.log("created a new post: " + post);
+    },
+    'removePost':function(id){
+      Posts.remove({_id:id});
+    },
+    'addUpVote': function(options){
+      var vote = {
+            owner: Meteor.userId(),
+            date: new Date(),
+            parent: options.parent
       };
-
-    Posts.insert(post);
-    console.log("created a new post: " + post);
-  },
-  'removePost':function(id){
-    Posts.remove({_id:id});
-  }
-});
+      UpVotes.insert(vote);
+    },
+    'addDownVote': function(options){
+      var vote = {
+            owner: Meteor.userId(),
+            date: new Date(),
+            parent: options.parent
+      };
+      DownVotes.insert(vote);
+    }
+  });
+}
